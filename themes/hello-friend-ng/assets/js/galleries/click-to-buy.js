57fcf2f6d2a23d502784efcfcd343df645c44b32
Duncan Mackenzie <duncanma@duncanmackenzie.net>
2024-04-28 13:43:09 -0700

    Adding purchasing as a feature, no content that uses it yet

---
+++ themes/hello-friend-ng/assets/js/galleries/click-to-buy.js
@@ -0,0 +1,39 @@
+
+const gallery = document.getElementById("gallery");
+const showBuyButtons = "showBuyButtons";
+if (gallery) {
+
+    const availableToPurchase = gallery.querySelectorAll(".availableForPurchase");
+
+    const visible = sessionStorage.getItem(showBuyButtons);
+
+    if (visible) {
+        document.body.classList.add(showBuyButtons);
+    }
+    else {
+        document.body.classList.remove(showBuyButtons);
+    }
+
+    availableToPurchase.forEach(element => {
+        element.onclick = function(e) {
+            e.preventDefault();
+            const body = document.body;
+            body.classList.toggle(showBuyButtons);
+            sessionStorage.setItem(showBuyButtons, body.classList.contains(showBuyButtons));
+            return false;
+        }
+
+    });
+
+    const clickToBuySpans = gallery.querySelectorAll(".clickToBuy");
+
+    clickToBuySpans.forEach(element => {
+        element.onclick = function(e) {
+            e.stopPropagation();
+            const url = this.dataset.paymentLink;
+            window.location=url;
+            return false;
+        }
+
+    });
+}
--- themes/hello-friend-ng/assets/js/main.js
+++ themes/hello-friend-ng/assets/js/main.js
@@ -1,3 +1,4 @@
 // Some code could be here ...
 //import "./galleries/gallery.js";
 import "./galleries/lightbox.js";
+import "./galleries/click-to-buy.js"
