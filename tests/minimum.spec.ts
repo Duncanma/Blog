ea5f395915a625c1ebdbdf3384712bccb3956cb5
Duncan Mackenzie <duncanma@duncanmackenzie.net>
2024-05-20 18:51:33 -0700

    Update tests

---
+++ tests/minimum.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect, Page } from '@playwright/test';
+
+test('Visual Diff About', async ({ page }) => {
+    await visualDiff(page, '/about/');
+});
+
+test('Visual Diff Text and Image', async ({ page }) => {
+    await visualDiff(page, '/blog/space-games/');
+});
+
+
+async function visualDiff(page: Page, url: string) {
+    await page.goto(url);
+
+    // Trigger loading of all images
+    for (const img of await page.locator('//img').all()) {
+        await img.scrollIntoViewIfNeeded();
+    }
+
+    // Set up listeners concurrently
+    await expect(page).toHaveScreenshot({ fullPage: true, timeout: 50000});
+}
